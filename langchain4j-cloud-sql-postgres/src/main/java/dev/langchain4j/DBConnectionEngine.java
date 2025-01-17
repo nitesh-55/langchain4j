d79d96ef1e44c207fd8af7aa772c98e215da3587
Nitesh <niteshsharma@google.com>
2025-01-17 23:15:53 +0000

    Adding dependency for io.grpc

--- langchain4j-cloud-sql-postgres/src/main/java/dev/langchain4j/DBConnectionEngine.java
+++ langchain4j-cloud-sql-postgres/src/main/java/dev/langchain4j/DBConnectionEngine.java
@@ -2,20 +2,14 @@ package dev.langchain4j;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import javax.sql.DataSource;
 import java.util.Properties;
-
+import javax.sql.DataSource;
 
 public class DBConnectionEngine extends ConnectionPoolFactory {
 
   private static final String DB_USER = System.getenv("DB_USER");
   private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
   private static final String DB_NAME = System.getenv("DB_NAME");
-  private static final String INSTANCE_HOST = System.getenv("INSTANCE_HOST");
-  private static final String DB_PORT = System.getenv("DB_PORT");
-  private static final String PROJECT_ID = System.getenv("PROJECT_ID");
-  private static final String REGION = System.getenv("us-central-1");
-  private static final String CLUSTER_ID = System.getenv("CLUSTER_ID");
   private static final String CONNECTION_NAME = System.getenv("POSTGRES_CONNECTION_NAME");
 
   public static DataSource createConnectionPool() {
@@ -28,9 +22,10 @@ public class DBConnectionEngine extends ConnectionPoolFactory {
     connProps.setProperty("password", DB_PASSWORD);
     connProps.setProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory");
     connProps.setProperty("cloudSqlInstance", CONNECTION_NAME);
-
+    config.setJdbcUrl(jdbcURL);
+    config.setDataSourceProperties(connProps);
     configureConnectionPool(config);
-    
+
     // Initialize the connection pool using the configuration object.
     return new HikariDataSource(config);
   }
