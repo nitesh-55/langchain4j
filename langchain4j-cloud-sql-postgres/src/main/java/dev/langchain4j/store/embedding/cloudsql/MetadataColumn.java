322c1c24527f443b1740593b821d0a152785a9b0
Nitesh S <168490626+nitesh-55@users.noreply.github.com>
2025-03-18 19:49:01 +0000

    Changes for cloud sql add function (#28)
    * Changes for Cloud sql search function
    
    * Changes for add function in cloud sql vector store
    
    * Update java docs for postgres embedding store

--- langchain4j-cloud-sql-postgres/src/main/java/dev/langchain4j/store/embedding/cloudsql/MetadataColumn.java
+++ langchain4j-cloud-sql-postgres/src/main/java/dev/langchain4j/store/embedding/cloudsql/MetadataColumn.java
@@ -2,17 +2,21 @@ package dev.langchain4j.engine;
 
 public class MetadataColumn {
 
-  private String name;
-  private String type;
-  private Boolean nullable;
+    private String name;
+    private String type;
+    private Boolean nullable;
 
-  public MetadataColumn(String name, String type, Boolean nullable) {
-    this.name = name;
-    this.type = type;
-    this.nullable = nullable;
-  }
+    public MetadataColumn(String name, String type, Boolean nullable) {
+        this.name = name;
+        this.type = type;
+        this.nullable = nullable;
+    }
 
-  public String generateColumnString() {
-    return String.format("%s %s %s", name, type, nullable ? "" : "NOT NULL");
-  }
+    public String generateColumnString() {
+        return String.format("%s %s %s", name, type, nullable ? "" : "NOT NULL");
+    }
+
+    public String getName() {
+        return name;
+    }
 }
